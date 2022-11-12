0d97ff40f93d5c4cc6e8f41cbd5c3c217cde6702
PanSzelescik <panszelescik@gmail.com>
2022-11-12 17:24:07 +0100

    Fabric Models and BlockStates Data Generators

--- fabric/src/main/java/pl/panszelescik/basicmachines/fabric/datagen/provider/client/BasicMachinesModelsProvider.java
+++ fabric/src/main/java/pl/panszelescik/basicmachines/fabric/datagen/provider/client/BasicMachinesModelsProvider.java
@@ -4,6 +4,12 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.ItemModelGenerators;
+import net.minecraft.data.models.model.ModelTemplates;
+import net.minecraft.data.models.model.TexturedModel;
+import pl.panszelescik.basicmachines.api.common.type.UpgradeType;
+import pl.panszelescik.basicmachines.init.BasicMachinesBlocks;
+import pl.panszelescik.basicmachines.init.BasicMachinesItems;
+import pl.panszelescik.basicmachines.init.BasicMachinesTypes;
 
 public class BasicMachinesModelsProvider extends FabricModelProvider {
 
@@ -13,9 +19,19 @@ public class BasicMachinesModelsProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
+        blockStateModelGenerator.createTrivialCube(BasicMachinesBlocks.IRON_CASING.get());
+
+        for (var machineType : BasicMachinesTypes.MACHINE_TYPES) {
+            blockStateModelGenerator.createFurnace(machineType.getBlock(), TexturedModel.ORIENTABLE_ONLY_TOP);
+        }
     }
 
     @Override
     public void generateItemModels(ItemModelGenerators itemModelGenerator) {
+        itemModelGenerator.generateFlatItem(BasicMachinesItems.BLANK_UPGRADE.get(), ModelTemplates.FLAT_ITEM);
+
+        for (var upgradeItem : UpgradeType.values()) {
+            itemModelGenerator.generateFlatItem(upgradeItem.getItem(), ModelTemplates.FLAT_ITEM);
+        }
     }
 }
